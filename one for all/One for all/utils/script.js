b6c792e3eea30d8084ca27d6da16889010ccedf2
Pranjalmahapatra <115134883+pranjalm1005@users.noreply.github.com>
2024-06-28 18:33:19 +0530

    Add files via upload

---
+++ one for all/One for all/utils/script.js
@@ -0,0 +1,80 @@
+import { GoogleGenerativeAI } from "https://esm.run/@google/generative-ai";
+import { marked } from "https://esm.run/marked";
+
+/**
+ * Returns a model instance.
+ *
+ * @param {GoogleGenerativeAI.ModelParams} params
+ * @returns {GoogleGenerativeAI.GenerativeModel}
+ */
+export async function getGenerativeModel(params) {
+  // Fetch API key from server
+  // If you need a new API key, get it from https://makersuite.google.com/app/apikey
+  const API_KEY = "AIzaSyAurjYs--I1Hos9wFEtwIDIStQniM5JKEs";
+
+  const genAI = new GoogleGenerativeAI(API_KEY);
+
+  // For text-only inputs, use the gemini-pro model
+  return genAI.getGenerativeModel(params);
+}
+
+/**
+ * Converts a File object to a GoogleGenerativeAI.Part object.
+ *
+ * @param {Blob} file
+ * @returns {GoogleGenerativeAI.Part}
+ */
+export async function fileToGenerativePart(file) {
+  const base64EncodedDataPromise = new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.onloadend = () => resolve(reader.result.split(",")[1]);
+    reader.readAsDataURL(file);
+  });
+  return {
+    inlineData: { data: await base64EncodedDataPromise, mimeType: file.type },
+  };
+}
+
+/**
+ * Scrolls the document all the way to the bottom.
+ */
+export function scrollToDocumentBottom() {
+  const scrollingElement = document.scrollingElement || document.body;
+  scrollingElement.scrollTop = scrollingElement.scrollHeight;
+}
+
+/**
+ * Updates the `resultEl` with parsed markdown text returned by a `getResult()` call.
+ *
+ * @param {HTMLElement}} resultEl
+ * @param {() => Promise<GoogleGenerativeAI.GenerateContentResponse>} getResult
+ * @param {boolean} streaming
+ */
+export async function updateUI(resultEl, getResult, streaming) {
+  resultEl.className = "loading";
+  let text = "";
+  try {
+    const result = await getResult();
+
+    if (streaming) {
+      resultEl.innerText = "";
+      for await (const chunk of result.stream) {
+        // Get first candidate's current text chunk
+        const chunkText = chunk.text();
+        text += chunkText;
+        resultEl.innerHTML = marked.parse(text);
+        scrollToDocumentBottom();
+      }
+    } else {
+      const response = await result.response;
+      text = response.text();
+    }
+
+    resultEl.className = ""; // Remove .loading class
+  } catch (err) {
+    text += "\n\n> " + err;
+    resultEl.className = "error";
+  }
+  resultEl.innerHTML = marked.parse(text);
+  scrollToDocumentBottom();
+}
